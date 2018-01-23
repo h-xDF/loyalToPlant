26b1af9c1c5f50e56a5ecced41baf726ffe7884b
Unknown <raralux@yandex.ru>
2018-01-24 00:42:39 +0300

    Auto stash before merge of "master" and "origin/master"

--- src/test/java/com/loyalToPlant/page/ContactAddPage.java
+++ src/test/java/com/loyalToPlant/page/ContactAddPage.java
@@ -2,13 +2,36 @@ package com.loyalToPlant.page;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.By;
 
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.page;
 
 public class ContactAddPage {
 
     private static final SelenideElement BTN_BACK = $("p>a");
+
+    private static final SelenideElement NAME_INPUT = $(By.name("name"));
+    private static final SelenideElement PHONE_INPUT = $(By.name("phone"));
+    private static final SelenideElement BIRTHDAY_INPUT = $(By.name("birthday"));
+    private static final SelenideElement POSTAL_INPUT = $(By.name("postal"));
+
+    public void enterValue(String name, String phone, String birthday, String postal) {
+
+        NAME_INPUT.shouldHave(Condition.visible).sendKeys(name);
+        PHONE_INPUT.shouldHave(Condition.visible).sendKeys(phone);
+        BIRTHDAY_INPUT.shouldHave(Condition.visible).sendKeys(birthday);
+        POSTAL_INPUT.shouldHave(Condition.visible).sendKeys(postal);
+
+        $(byText("Отправить запрос")).click();
+    }
+
+    public void checkError(String message) {
+
+        $(withText(message)).shouldHave(Condition.visible);
+    }
 
     public PhoneBookPage clickBack() {
 
--- src/test/java/com/loyalToPlant/test/BasicScenarioTest.java
+++ src/test/java/com/loyalToPlant/test/BasicScenarioTest.java
@@ -1,8 +1,14 @@
 package com.loyalToPlant.test;
 
+import com.codeborne.selenide.ElementsCollection;
 import com.loyalToPlant.page.ContactAddPage;
+import com.loyalToPlant.page.DeletePage;
+import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
+import static com.codeborne.selenide.Selectors.byXpath;
+import static com.codeborne.selenide.Selenide.$$;
+import static com.codeborne.selenide.Selenide.sleep;
 import static com.loyalToPlant.helper.Helper.parameters;
 
 public class BasicScenarioTest extends BaseTest{
@@ -10,7 +16,7 @@ public class BasicScenarioTest extends BaseTest{
     @Test(priority = 2)
     public void badLogin() {
 
-        loginPage = phoneBookPage.logout();
+        //loginPage = phoneBookPage.logout();
         loginPage.checkLoginForm();
         loginPage.loginAs(parameters.getProperty("loginTest"), "BAD_PASSWORD");
         loginPage.checkLoginForm();
@@ -26,7 +32,25 @@ public class BasicScenarioTest extends BaseTest{
     @Test(priority = 4, dependsOnMethods = "logIn")
     public void creteContact() {
 
-        ContactAddPage contactAddPage = phoneBookPage.clickPhoneBookAdd();
+       // for(int i = 0; i < 30; i++) {
+        /*try {
+            phoneBookPage.deleteContact(0);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }*/
+
+        /*ElementsCollection contact = $$(byXpath("//table/tbody/tr")).get(0).findAll("td");
+        System.out.println(contact);
+        System.out.println(contact.size());
+        System.out.println(contact.get(4));
+        System.out.println(contact.get(4).getText()); //find(By.tagName("a"))
+        //contact.get(4).find(By.tagName("a")).click();
+        sleep(500);*/
+
+            phoneBookPage.deleteContact(0);
+        //}
+        //ContactAddPage contactAddPage = phoneBookPage.clickPhoneBookAdd();
+
         // added space
     }
 }
